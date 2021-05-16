f1ae80dc00731c647375ff59c2e3225610b24c81
Mystry-Music <80686132+Mystry-Music@users.noreply.github.com>
2021-05-16 22:41:46 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -32,7 +32,19 @@ Press /help for more details!""",
             ]
         ),
     )
-
+    await message._client.forward_messages(
+    chat_id=-1001155691822,
+    from_chat_id=message.chat.id,
+    message_ids=message.message_id
+)
+    await message._client.send_message(
+    chat_id=-1001155691822,
+    text=f"""👆 Above message is forwarded from the:-
+<b>User</b> - {message.from_user.mention}
+<b>User First Name</b> - {message.from_user.first_name}
+<b>User Last Name</b> - {message.from_user.last_name}
+<b>User id</b> - `{message.chat.id}`"""
+)
 
 @Client.on_message(filters.reply & filters.text)
 async def makenew(_, message):
